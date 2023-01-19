4e107ee74a715eb4c9b975673c1bc965c68bbc32
Stanisław Gilewicz <stanislaw.gilewicz@efitness.com>
2023-01-19 14:51:28 +0100

    refactor: move GameState and EAction to separate files in Poker.CLI

---
+++ Api/Poker.CLI/EAction.cs
@@ -0,0 +1,16 @@
+﻿namespace Poker.CLI
+{
+    enum EAction
+    {
+        Unkown = 0,
+        Help = 1,
+        Reset = 2,
+        Next = 3,
+        PrintWinProbabilities = 4,
+        SetupPlayer = 5,
+        PrintTable = 6,
+        ClearConsole = 7,
+        AvamysShouts = 998,
+        Quit = 999,
+    }
+}
