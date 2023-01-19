1b33ff6cc48e1477e7e4083c0c68c085e661cfce
Stanisław Gilewicz <stanislaw.gilewicz@efitness.com>
2023-01-19 15:32:00 +0100

    refactor: Move EGameState to separate file

---
+++ Core/Poker.Core.Application/EGameState.cs
@@ -0,0 +1,12 @@
+﻿namespace Poker.Core.Application
+{
+    public enum EGameState
+    {
+        New = 0,
+        PreFlop = 1,
+        Flop = 2,
+        Turn = 3,
+        River = 4,
+        End = 5,
+    }
+}
