9c4a0d0dcfea384e3273f01430992c8b14d41ed3
Stanisław Gilewicz <stanislaw.gilewicz@efitness.com>
2023-01-20 20:08:35 +0100

    test: add simple integration test for StandardGame

---
+++ Tests/Poker.Tests/IntegrationTests/StandardGameTests.cs
@@ -0,0 +1,64 @@
+﻿using NSubstitute;
+using Poker.Core.Application;
+using Poker.Core.Application.CombinationsLogic;
+using Poker.Core.Application.Events;
+using Poker.Core.Application.GameBehaviour;
+using Poker.Core.Domain.Entity;
+using Xunit;
+
+namespace Poker.Tests.IntegrationTests
+{
+    public class StandardGameTests
+    {
+        private readonly StandardGame _game;
+        private const string Player1Name = "Player1";
+        private const string Player2Name = "Player2";
+        private const string Player3Name = "Player3";
+        private const string Player4Name = "Player4";
+
+        public StandardGameTests()
+        {
+            var combinationComparer = new CombinationComparer();
+            var winChanceEstimator = new WinChanceEstimator();
+            var table = new Table();
+            var eventPublisher = Substitute.For<IEventPublisher>();
+            var players = new Players()
+            {
+                new Player(Player1Name),
+                new Player(Player2Name),
+                new Player(Player3Name),
+                new Player(Player4Name),
+            };
+            _game = new StandardGame(combinationComparer,
+                                     winChanceEstimator,
+                                     table,
+                                     players,
+                                     eventPublisher);
+        }
+
+        [Theory]
+        [InlineData("Ac;Ah", "9h;8h", "As;Js", "3h;4h", "Kh;Qh;Jh", "10h", "3h", Player1Name)]
+        [InlineData("2h;Ah", "9h;8h", "As;Js", "3h;4h", "Kh;Qh;Jh", "10h", "3h", Player1Name)]
+        public void Should_properly_play_the_game_and_select_proper_winners(string targetPlayerCards,
+            string player2Cards,
+            string player3Cards,
+            string player4Cards,
+            string flop,
+            string turn,
+            string river,
+            string expectedWinnerName)
+        {
+            _game.ResetRound();
+            _game.InsertTargetPlayerCards(targetPlayerCards);
+            _game.Flop(flop);
+            _game.Turn(turn);
+            _game.River(river);
+            _game.FillPlayersCards(Player2Name, player2Cards);
+            _game.FillPlayersCards(Player3Name, player3Cards);
+            _game.FillPlayersCards(Player4Name, player4Cards);
+            _game.EndRound();
+
+            Assert.True(_game.GetWinnersAsString().Contains(expectedWinnerName));
+        }
+    }
+}
