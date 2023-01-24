88d2e7e1e4096dea68628344f410dc18135c3acc
Stanisław Gilewicz <stanislaw.gilewicz@efitness.com>
2023-01-24 15:16:27 +0100

    refactor: move Pot to separate file

--- Core/Poker.Core.Application/Betting/BetOverseer.cs
+++ Core/Poker.Core.Application/Betting/BetOverseer.cs
@@ -1,7 +1,5 @@
 ﻿using Poker.Core.Domain.Entity;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Threading;
 
@@ -112,41 +110,6 @@ namespace Poker.Core.Application.Betting
         public string GetBigBlindPlayer()
         {
             throw new NotImplementedException();
-        }
-    }
-
-    public class Pot
-    {
-        private readonly List<(string PlayerName, int Amount)> _transactions;
-
-        public Pot()
-        {
-            _transactions = new List<(string PlayerName, int Amount)>();
-        }
-
-        public int AmountToCheck(string playerName)
-        {
-            var sumOfPlayerBets = _transactions
-                .GroupBy(x => x.PlayerName)
-                .SingleOrDefault(x => x.Key == playerName)
-                ?.Sum(x => x.Amount) ?? 0;
-
-            var highestBet = _transactions
-                .GroupBy(x => x.PlayerName)
-                .Select(x => x.Sum(x => x.Amount))
-                .OrderByDescending(x => x)
-                .FirstOrDefault();
-
-            return highestBet - sumOfPlayerBets;
-        }
-        public void AddToPot(string playerName, int amount)
-        {
-            _transactions.Add((playerName, amount));
-        }
-
-        public int GetTotalAmount()
-        {
-            return _transactions.Sum(x => x.Amount);
         }
     }
 }
---
+++ Core/Poker.Core.Application/Betting/Pot.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Poker.Core.Application.Betting
+{
+    public class Pot
+    {
+        private readonly List<(string PlayerName, int Amount)> _transactions;
+
+        public Pot()
+        {
+            _transactions = new List<(string PlayerName, int Amount)>();
+        }
+
+        public int AmountToCheck(string playerName)
+        {
+            var sumOfPlayerBets = _transactions
+                .GroupBy(x => x.PlayerName)
+                .SingleOrDefault(x => x.Key == playerName)
+                ?.Sum(x => x.Amount) ?? 0;
+
+            var highestBet = _transactions
+                .GroupBy(x => x.PlayerName)
+                .Select(x => x.Sum(x => x.Amount))
+                .OrderByDescending(x => x)
+                .FirstOrDefault();
+
+            return highestBet - sumOfPlayerBets;
+        }
+        public void AddToPot(string playerName, int amount)
+        {
+            _transactions.Add((playerName, amount));
+        }
+
+        public int GetTotalAmount()
+        {
+            return _transactions.Sum(x => x.Amount);
+        }
+    }
+}
